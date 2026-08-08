eacff46b6c14b5b2329a346cf4621d593c3a2f92
saimonlenui2002-hash <saimonlenui2002@gmail.com>
2026-08-08 22:26:36 +0700

    Add app Gradle build

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.pooltrajectory"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.pooltrajectory"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("org.opencv:opencv:4.13.0")
+}
