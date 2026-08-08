2456ac1c22eadf0c35ed53d0307c5ccca77ec028
saimonlenui2002-hash <saimonlenui2002@gmail.com>
2026-08-09 00:11:41 +0700

    Release guideline endpoint build 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.pooltrajectory"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "2.1.0"
     }
 
     buildTypes {
