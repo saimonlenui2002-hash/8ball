d01db485fc2bc8a94892db0e9cb095a2ccb35512
saimonlenui2002-hash <saimonlenui2002@gmail.com>
2026-08-10 23:20:50 +0700

    Bump app version to 4.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.pooltrajectory"
         minSdk = 29
         targetSdk = 36
-        versionCode = 13
-        versionName = "4.2.0"
+        versionCode = 14
+        versionName = "4.2.1"
     }
 
     buildTypes {
