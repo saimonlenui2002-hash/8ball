48e4ac296348dd8cb99387216cc2440d6970ff89
saimonlenui2002-hash <saimonlenui2002@gmail.com>
2026-08-12 00:12:40 +0700

    Bump app version to 6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.pooltrajectory"
         minSdk = 29
         targetSdk = 36
-        versionCode = 15
-        versionName = "5.0"
+        versionCode = 16
+        versionName = "6.0"
     }
 
     buildTypes {
